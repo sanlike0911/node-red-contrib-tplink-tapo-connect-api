d04bf101c4c187d07d1f61edca9956827b1a7b05
hisashi <sanlike0911@gmail.com>
2021-08-30 15:46:34 +0900

    chg: project files layout

--- build.bat
+++ build.bat
@@ -4,12 +4,9 @@ set CurrentDirectory="%~dp0"
 
 rem build and install and npm run start
 npm run build & ^
-xcopy /Y /S /E src\locales\ nodes\locales\ & ^
-copy /Y src\*.html nodes\ & ^
-xcopy /Y /S /E dist\* nodes\* & ^
-md data\your-node\nodes\ & ^
-copy /Y package.json data\your-node\ & ^
-xcopy /Y /S /E nodes\* data\your-node\nodes\* & ^
+xcopy /Y /S /E /I /EXCLUDE:build-exclude-filelist src\nodes\* dist\* & ^
+xcopy /Y /S /E /I dist\* data\your-node\nodes\* & ^
+xcopy /Y /I package.json data\your-node\* & ^
 cd data/ & ^
 npm install .\your-node & ^
 cd .. & ^
