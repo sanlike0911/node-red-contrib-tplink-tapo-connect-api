ed684215eebb29d68f659f6e122fa56394962dc3
hisashi <sanlike0911@gmail.com>
2021-09-10 08:08:15 +0900

    Fix: Missing files

--- build.bat
+++ build.bat
@@ -4,8 +4,9 @@ set CurrentDirectory="%~dp0"
 
 rem build and install and npm run start
 npm run build & ^
-xcopy /Y /S /E /I /EXCLUDE:build-exclude-filelist src\nodes\* dist\* & ^
-xcopy /Y /S /E /I dist\* data\your-node\nodes\* & ^
+xcopy /Y /S /E /I /EXCLUDE:build-exclude-filelist src\nodes\* nodes\* & ^
+xcopy /Y /S /E /I dist\* nodes\* & ^
+xcopy /Y /S /E /I nodes\* data\your-node\nodes\* & ^
 xcopy /Y /I package.json data\your-node\* & ^
 cd data/ & ^
 npm install .\your-node & ^
